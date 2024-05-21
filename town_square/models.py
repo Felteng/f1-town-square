7aed97a25c8edc227c9d9ef4cbce690842ee21c1
Felix Tenggren <felixtenggren1@gmail.com>
2024-05-21 06:43:06 +0000

    Add race_event model

--- town_square/models.py
+++ town_square/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class RaceEvent(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    location = models.CharField()
+    event_name = models.CharField()
+    event_image = CloudinaryField('image', default='event_placeholder')
+
+
+# Code source: https://github.com/Code-Institute-Solutions/Django3blog/blob/75bd87f4439d678bee07c149383cf2d778c38a6f/11_messages/blog/models.py#L9-L25
 class Article(models.Model):
     title = models.CharField(max_length=80, unique=True)
     slug = models.SlugField(max_length=80, unique=True)
