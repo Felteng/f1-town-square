3f9fe89ce5fe1d647725f7d5f6262964b5efb41a
Felix Tenggren <felixtenggren1@gmail.com>
2024-05-25 10:04:40 +0000

    Fix broken tuple

--- event_details/admin.py
+++ event_details/admin.py
@@ -5,5 +5,5 @@ from .models import RaceEventDetail
 # Register your models here.
 @admin.register(RaceEventDetail)
 class articleAdmin(SummernoteModelAdmin):
-  list_display = ('race')
+  list_display = ('race',)
   summernote_fields = ('event_info')
