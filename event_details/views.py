ab8000c94aeeba29fa3824d1480f439049ceb933
Felix Tenggren <felixtenggren1@gmail.com>
2024-05-25 10:11:07 +0000

    Add calculation for race_distance to view

--- event_details/views.py
+++ event_details/views.py
@@ -5,11 +5,13 @@ from .models import RaceEventDetail
 def event_details(request, event_id):
     queryset = RaceEventDetail.objects.filter(race=event_id)
     event = get_object_or_404(queryset)
+    race_distance = event.circuit_length * event.number_of_laps
 
     return render(
         request,
         'event_details/event_details.html',
         {
             'event': event,
+            'race_distance': race_distance,
         }
     )
