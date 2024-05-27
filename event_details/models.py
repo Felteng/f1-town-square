137fcaab42ed904a6a172dda139036a4444bfb31
Felix Tenggren <felixtenggren1@gmail.com>
2024-05-27 12:28:41 +0000

    Add RaceEventComment db model

--- event_details/models.py
+++ event_details/models.py
@@ -8,9 +8,21 @@ class RaceEventDetail(models.Model):
     race = models.OneToOneField(
         RaceEvent, on_delete=models.CASCADE, related_name='event_details'
     )
-    event_hero_image = CloudinaryField('Hero image', default="image/upload/v1716558951/checkered-flags.svg")
+    event_hero_image = CloudinaryField('Hero image', default='image/upload/v1716558951/checkered-flags.svg')
     event_info = models.TextField()
     circuit_name = models.CharField()
     circuit_length = models.FloatField(default=0)
     number_of_laps = models.PositiveIntegerField(default=0)
-    lap_record = models.CharField('Lap record (x:xx.xxx)', max_length=8, default="x:xx.xxx")
+    lap_record = models.CharField('Lap record (x:xx.xxx)', max_length=8, default='x:xx.xxx')
+
+
+class RaceEventComment(models.Model):
+    event = models.ForeignKey(
+        RaceEventDetail, on_delete=models.CASCADE, related_name='comments'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='event_comments'
+    )
+    comment = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
