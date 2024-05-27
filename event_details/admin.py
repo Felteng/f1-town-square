d01c48aafaca947c1dc91343da8df1c68b09c092
Felix Tenggren <felixtenggren1@gmail.com>
2024-05-27 14:04:05 +0000

    Integrate race event comments

--- event_details/admin.py
+++ event_details/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import RaceEventDetail
+from .models import RaceEventDetail, RaceEventComment
 
 # Register your models here.
 @admin.register(RaceEventDetail)
 class articleAdmin(SummernoteModelAdmin):
-  list_display = ('race',)
+  list_display = ('race', 'circuit_name')
   summernote_fields = ('event_info')
+
+@admin.register(RaceEventComment)
+class commentAdmin(SummernoteModelAdmin):
+  list_display = ('comment', 'author', 'approved')
+  list_filter = ('approved',)
+  
--- event_details/views.py
+++ event_details/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import RaceEventDetail
+from .models import RaceEventDetail, RaceEventComment
 
 # Create your views here.
 def event_details(request, event_id):
@@ -7,11 +7,14 @@ def event_details(request, event_id):
     event = get_object_or_404(queryset)
     race_distance = round(event.circuit_length * event.number_of_laps, 2)
 
+    comments = event.comments.all()
+
     return render(
         request,
         'event_details/event_details.html',
         {
             'event': event,
+            'comments': comments,
             'race_distance': race_distance,
         }
     )
