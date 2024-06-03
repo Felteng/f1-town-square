dbb74845b03724715aeaf813ed6559ef96b2d41d
Felix Tenggren <felixtenggren1@gmail.com>
2024-06-03 13:26:17 +0000

    Add check for non empty input

--- static/js/live_chat.js
+++ static/js/live_chat.js
@@ -3,32 +3,53 @@ const chatSocket = new WebSocket("wss://" + window.location.host + "/");
 const liveChatContainer = document.querySelector("#id-chat-item-container");
 const chatAnchor = document.querySelector("#chat-anchor");
 
+
 chatSocket.onopen = function () {
   let div = document.createElement("div");
   div.innerHTML = "Connected to chat!";
   liveChatContainer.insertBefore(div, chatAnchor.nextSibling);
 };
+
+
 chatSocket.onclose = function () {
   let div = document.createElement("div");
-  div.innerHTML =("Connection to chat has closed due to inactivity!"
-                + "<br/>Refresh page to reconnect");
+  div.innerHTML =(`Connection to chat has closed due to inactivity!
+    <br/>Refresh page to reconnect`
+  );
+
   liveChatContainer.insertBefore(div, chatAnchor.nextSibling);
 };
+
+
 document.querySelector("#id_message_send_input").focus();
 document.querySelector("#id_message_send_input").onkeyup = function (e) {
   if (e.keyCode == 13) {
     document.querySelector("#id_message_send_button").click();
   }
 };
+
+
 document.querySelector("#id_message_send_button").onclick = function () {
+  /**
+   * Custom addition of 'if (/[a-z0-9]/i.test(messageInput))'
+   * used to check if the input contains any alphanumerical
+   * characters to not have the chat spammed with empty 
+   * strings of text.
+   * 
+   * Username variable is defined in the template using DTL.
+   */
   let messageInput = document.querySelector(
     "#id_message_send_input"
   ).value;
-  chatSocket.send(JSON.stringify({
+  if (/[a-z0-9]/i.test(messageInput)) {
+    chatSocket.send(JSON.stringify({
     message: messageInput,
-    username: username
+    username: username,
   }));
+  }
 };
+
+
 chatSocket.onmessage = function (e) {
   const data = JSON.parse(e.data);
   let div = document.createElement("div");
