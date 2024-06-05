6e562c359e7e75a971d315dbfdf02495adf98031
Felix Tenggren <felixtenggren1@gmail.com>
2024-06-05 14:53:05 +0000

    Format code per pep8 standards

--- event_details/views.py
+++ event_details/views.py
@@ -14,8 +14,8 @@ def event_details(request, event_id):
     comment_count = comments.filter(approved=True).count()
 
     # https://stackoverflow.com/a/57682143: forcing https for images.
-    event.event_hero_image.url_options.update({'secure':True})
-    event.race.event_circuit.url_options.update({'secure':True})
+    event.event_hero_image.url_options.update({'secure': True})
+    event.race.event_circuit.url_options.update({'secure': True})
 
     if request.method == "POST":
         race_comment_form = RaceCommentForm(data=request.POST)
@@ -24,9 +24,17 @@ def event_details(request, event_id):
             comment.author = request.user
             comment.event = event
             comment.save()
-            messages.add_message(request, messages.SUCCESS, "Your comment has been added.")
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Your comment has been added."
+            )
         else:
-            messages.add_message(request, messages.ERROR, "Something went wrong, your comment could not be added.")
+            messages.add_message(
+                request,
+                messages.ERROR,
+                "Something went wrong, your comment could not be added."
+            )
 
     race_comment_form = RaceCommentForm()
 
@@ -58,10 +66,18 @@ def approve_comment(request, event_id, target_comment):
     if request.user.is_superuser:
         comment.approved = True
         comment.save()
-        messages.add_message(request, messages.SUCCESS, "Comment approved successfully.")
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            "Comment approved successfully."
+        )
     else:
-        messages.add_message(request, messages.ERROR, "Comment could not be approved, are you a site admin?")
-    
+        messages.add_message(
+            request,
+            messages.ERROR,
+            "Comment could not be approved, are you a site admin?"
+        )
+
     return HttpResponseRedirect(reverse('event_details', args=[event_id]))
 
 
@@ -79,9 +95,17 @@ def delete_comment(request, event_id, target_comment):
 
     if request.user == comment.author:
         comment.delete()
-        messages.add_message(request, messages.SUCCESS, "Comment deleted successfully.")
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            "Comment deleted successfully."
+        )
     else:
-        messages.add_message(request, messages.ERROR, "Comment could not be deleted, are you the author?")
+        messages.add_message(
+            request,
+            messages.ERROR,
+            "Comment could not be deleted, are you the author?"
+        )
 
     return HttpResponseRedirect(reverse('event_details', args=[event_id]))
 
@@ -105,8 +129,16 @@ def edit_comment(request, event_id, target_comment):
             comment = race_comment_form.save(commit=False)
             comment.approved = False
             comment.save()
-            messages.add_message(request, messages.SUCCESS, "Comment edited successfully.")
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Comment edited successfully."
+            )
         else:
-            messages.add_message(request, messages.ERROR, "Comment could not be edited, are you the author?")
+            messages.add_message(
+                request,
+                messages.ERROR,
+                "Comment could not be edited, are you the author?"
+            )
 
     return HttpResponseRedirect(reverse('event_details', args=[event_id]))
